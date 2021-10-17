60f99b6cca5a1df7972bb3f752c841ed7afea191
breyshaw <dawsonbrey@gmail.com>
2021-10-16 21:45:30 -0400

    adding more variables

--- js/app.js
+++ js/app.js
@@ -7,6 +7,9 @@ let isWinner, startBattle, lose
 const answerInput = document.querySelector("#answer-input")
 const messageEl = document.querySelector("#message")
 const resetBtn = document.querySelector("#startOver-button")
+const path1Btn = document.querySelector("#path-one")
+const path2Btn = document.querySelector("#path-two")
+
 /*------------------------ Cached Element References ------------------------*/
 
 
