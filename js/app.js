ced5daaa0c33de2e43afde8fb253815eec4efac1
breyshaw <dawsonbrey@gmail.com>
2021-10-16 22:06:37 -0400

    aworking init function

--- js/app.js
+++ js/app.js
@@ -9,7 +9,11 @@ const messageEl = document.querySelector("#message")
 const resetBtn = document.querySelector("#startOver-button")
 const path1Btn = document.querySelector("#path-one")
 const path2Btn = document.querySelector("#path-two")
-
+const enemyHel = document.querySelector("#enemyHealth")
+const playerHel = document.querySelector("#playerHealth")
+const enemyImg = document.querySelector("#enemy")
+const pathOneImg = document.querySelector("#pathOneImg")
+const pathTwoImg = document.querySelector("#pathOneImg")
 /*------------------------ Cached Element References ------------------------*/
 
 
@@ -19,3 +23,17 @@ const path2Btn = document.querySelector("#path-two")
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+init()
+
+function init() {
+    messageEl.innerText = 'Choose a path..'
+    resetBtn.setAttribute('hidden', true)
+    enemyHel.setAttribute('hidden', true)
+    playerHel.setAttribute('hidden', true)
+    enemyImg.setAttribute('hidden', true)
+    pathOneImg.setAttribute('hidden', false)
+    pathTwoImg.setAttribute('hidden' , false)
+    isWinner = false
+    isLoser = false
+}
