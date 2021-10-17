c1606627283db43dc987818f425769e25900e3c7
breyshaw <dawsonbrey@gmail.com>
2021-10-16 21:41:52 -0400

    defining constants and variables

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 /*-------------------------------- Constants --------------------------------*/
-
+let isWinner, startBattle, lose
 
 
 /*-------------------------------- Variables --------------------------------*/
 
-
-
+const answerInput = document.querySelector("#answer-input")
+const messageEl = document.querySelector("#message")
+const resetBtn = document.querySelector("#startOver-button")
 /*------------------------ Cached Element References ------------------------*/
 
 
