81191273f1a9488a8de6960ff96d5dfc998c6491
breyshaw <dawsonbrey@gmail.com>
2021-10-17 01:06:19 -0400

    debugging and working functions

--- js/app.js
+++ js/app.js
@@ -4,9 +4,11 @@
 
 /*-------------------------------- Variables --------------------------------*/
 
-let isWinner, isLoser
+let isWinner, isPlaying, isLoser
 
 /*------------------------ Cached Element References ------------------------*/
+
+const form = document.querySelector('#form')
 const answerInput = document.querySelector("#answer-input")
 const messageEl = document.querySelector("#message")
 const resetBtn = document.querySelector("#startOver-button")
@@ -23,14 +25,24 @@ const pathTwoImg = document.querySelector("#pathOneImg")
 
 form.addEventListener('reset', init)
 resetBtn.addEventListener('click', init)
-path1Btn.addEventListener('click') //callback needs to be added)
-path2Btn.addEventListener('click')//callback function needed
+//Need to ask if I can use this inBattle like this
+//Or if I need to take the route going with path2Btn
+path1Btn.addEventListener('click', inBattle)
+// path2Btn.addEventListener('click', function (evt) {
+//     evt.preventDefault()
+//     if (isWinner === false) {
+//     messageEl = randomQuestion()
+//     enemyHel.removeAttribute('hidden')
+//     playerHel.removeAttribute('hidden')
+//     enemyImg.removeAttribute('hidden')
+    // }
+// })
 
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-messageEl.innerText = ''
+
 
 init()
 
@@ -40,14 +52,15 @@ function init() {
     // enemyHel.setAttribute('hidden', true)
     // playerHel.setAttribute('hidden', true)
     // enemyImg.setAttribute('hidden', true)
-    //keeping this line in case inBattle does not "unhide" the elements when tested
+    //keeping these lines in case inBattle does not "unhide" the elements when tested
     pathOneImg.setAttribute('hidden', false)
     pathTwoImg.setAttribute('hidden', false)
     isWinner = false
     isLoser = false
+    isPlaying = true
     inBattle() = false
-    playerHel = 100/100
-    enemyHel = 100/100
+    playerHel = (100 / 100)
+    enemyHel = (100 / 100)
     render()
 }
 
@@ -56,7 +69,6 @@ function inBattle() {
     enemyHel.setAttribute('hidden', false)
     playerHel.setAttribute('hidden', false)
     enemyImg.setAttribute('hidden', false)
-    render()
 }
 
 function randomQuestion() {
@@ -69,18 +81,40 @@ function renderAns(lastAns) {
     if (lastAns !== correctAns) {
         messageEl.className = 'damage'//to style the text later
         messageEl.innerText = `${lastAns} is wrong! You have taken damage!`
-        playerHel = playerHel - 20/100
+        playerHel = playerHel - 20 / 100
     } else {
         messageEl.className = 'attack'//to style the text later
         messageEl.innerText = `${lastAns} is correct! You cast fireball!!`
-        enemyHel = enemyHel - 20/100
+        enemyHel = enemyHel - 20 / 100
     }
 }
 
-function renderPath1() {
+function renderPath() {
+    messageEl.innerText = 'Choose a path..'
+    resetBtn.setAttribute('hidden', true)
+    pathOneImg.setAttribute('hidden', false)
+    pathTwoImg.setAttribute('hidden', false)
+    isWinner = false
+    isLoser = false
+    isPlaying = true
+    inBattle() = false
+}
+
+function renderWin() {
 
 }
 
-function renderPath2() {
+function renderDied() {
 
+}
+
+function render() {
+    if (isPlaying = true) {
+        renderPath()
+    } else if (isWinner = true) {
+        renderWin()//render winning screen?
+    }
+    if (isLoser = true) {
+        renderDied()//render losing screen?
+    }
 }
